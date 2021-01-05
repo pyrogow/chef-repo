902125dfa6c431bf50f140069d04d2e1a05315e7
pyrogow <pyrogow@gmail.com>
2021-01-05 11:37:45 +0200

    add files

--- cookbooks/selinux/recipes/httpd_true.rb
+++ cookbooks/selinux/recipes/httpd_true.rb
@@ -1,7 +1,13 @@
-include_recipe 'selinux_policy::install'
+#include_recipe 'selinux_policy::install'
 
-selinux_policy_boolean 'httpd_can_network_connect' do
+# selinux_policy_boolean 'httpd_can_network_connect' do
+#     value true
+#     # Make sure nginx is started if this value was modified
+#     notifies :restart,'service[nginx]', :immediate
+# end
+
+selinux_policy 'httpd_can_network_connect' do
     value true
     # Make sure nginx is started if this value was modified
-    notifies :restart,'service[nginx]', :immediate
+    # notifies :restart,'service[nginx]', :immediate
 end
