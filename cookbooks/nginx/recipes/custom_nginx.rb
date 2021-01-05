a9076638376e2fb49f927e1d64ed5369a9ade2e1
pyrogow <pyrogow@gmail.com>
2021-01-05 11:41:46 +0200

    add files

--- cookbooks/nginx/recipes/custom_nginx.rb
+++ cookbooks/nginx/recipes/custom_nginx.rb
@@ -1,16 +1,16 @@
-include_recipe "apt"
+# include_recipe "apt"
 
-apt_repository 'nginx' do
-  uri          'http://nginx.org/packages/ubuntu/'
-  distribution node['lsb']['codename']
-  components   ['nginx']
-  key          'http://nginx.org/keys/nginx_signing.key'
-  deb_src      true
-end
+# apt_repository 'nginx' do
+#   uri          'http://nginx.org/packages/ubuntu/'
+#   distribution node['lsb']['codename']
+#   components   ['nginx']
+#   key          'http://nginx.org/keys/nginx_signing.key'
+#   deb_src      true
+# end
 
-apt_package 'nginx' do
-  action :install
-end
+# apt_package 'nginx' do
+#   action :install
+# end
 
 file "/etc/nginx/nginx.conf" do
    action :delete
