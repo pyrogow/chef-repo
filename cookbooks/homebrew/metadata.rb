098b3e85c0bea28162ee27d66e48f8870c858417
pyrogow <pyrogow@gmail.com>
2021-02-02 10:43:06 +0200

    Import homebrew version 5.2.0

---
+++ cookbooks/homebrew/metadata.rb
@@ -0,0 +1,11 @@
+name             'homebrew'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license          'Apache-2.0'
+description      'Install Homebrew and includes resources for working with taps and casks'
+version          '5.2.0'
+supports         'mac_os_x'
+
+source_url 'https://github.com/chef-cookbooks/homebrew'
+issues_url 'https://github.com/chef-cookbooks/homebrew/issues'
+chef_version '>= 12.15'
